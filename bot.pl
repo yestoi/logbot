c74ebe9f78ef66fca23e82711aa28fe3bdf8aa80
yestoi <yestoi@myhost.(none)>
2009-10-27 17:26:46 -0400

    Final product

--- bot.pl
+++ bot.pl
@@ -4,26 +4,27 @@ use IO::Socket;
 #Do config lookup
 my $nick = "logbot";
 my $server = "irc.undergroundsystems.org";
-my $channel = "#underground_systems";
-my $pass = "IAMAMADDAFAKKINGLOGBOT"
+my $channel = "#HaKT_Dev";
+my $pass = "IAMAMADDAFAKKINGLOGBOT";
 
 my $sock = new IO::Socket::INET(PeerAddr => $server,
-								PeerPort => 6667,
-								Proto => 'tcp') or
-									die "Put my socks on!";
+                                PeerPort => 6667,
+                                Proto => 'tcp') or
+                                        die "Put my socks on!";
 print $sock "NICK $nick\r\n";
 print $sock "USER $nick 8 * :$nick\r\n";
 
 while (my $in = <$sock>) {
-	if ($in =~ /004/) {	
+	if ($in =~ /266/) {	
 		last;
 	}	
 	elsif ($in =~ /433/) {  #Check if nick is in use
-		die "Who dat?"
+		die "Who dat?";
 	}
 }
 
-print $sock "MSG NickServ $pass"
+print $sock "PRIVMSG NickServ IDENTIFY $pass\r\n";
+sleep 3;
 print $sock "JOIN $channel\r\n";
 
 while (my $lines = <$sock>) {
