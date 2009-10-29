62f3b99d3827fb4e359b8eee2884e97faf7ecdfa
yestoi <yestoi@thunder.localdomain>
2009-10-29 15:22:48 -0400

    added command

--- bot.pl
+++ bot.pl
@@ -43,6 +43,9 @@ while (my $line = <$sock>)
 		if ($line =~ /^:(.+)!.*:(.+)$/) {
             push (@buffer, [$1, $2]);
             #print "<" .$1. "> " . $2 . "\n";
+            if ($2 ~= /^!$nick/) {
+                print $sock "PRIVMSG $channel Logs: ftp://hakt:GiveMEmaLOG@crazzy.se\r\n";
+            }
 		}
 	}
 
